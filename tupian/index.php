f716e6a150aefe879af46433141f5f6da86a645a
shangyunvip <shangyunvip@outlook.com>
2024-02-11 04:55:57 +0000

    update tupian/index.php.
    Signed-off-by: shangyunvip <shangyunvip@outlook.com>

--- tupian/index.php
+++ tupian/index.php
@@ -1,6 +1,28 @@
 <?php
-$img_array = glob('image/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
-if(count($img_array) == 0) die('没找到图片文件。请先上传一些图片到 '.dirname(__FILE__).'/images/ 文件夹');
-header('Content-Type:image/png');
-echo(file_get_contents($img_array[array_rand($img_array)]));
+//存有美图链接的文件名img.txt
+$filename = "img.txt";
+if(!file_exists($filename)){
+   die('文件不存在');
+}
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+   $line=trim(fgets($fs));
+   if($line!=''){
+       array_push($pics, $line);
+   }
+}
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+//JSON返回
+case 'json':
+   header('Content-type:text/json');
+   die(json_encode(['pic'=>$pic]));
+default:
+   die(header("Location: $pic"));
+}
 ?>
